6fa02434956a38412bf8cae8c69e92460566fe42
captGouZ <julien.gazeaurousseau@gmail.com>
2021-07-20 10:24:39 +0200

    Ajout évènements

---
+++ src/03-Evenements/demo1.js
@@ -0,0 +1,39 @@
+//region Code excuté qu'à la fin du chargement de la page.
+window.addEventListener('load', init);
+function init(){
+    var el = document.getElementById('saisie');
+    console.log(el);
+    document.querySelector('#btn1').addEventListener('click', validerSaisie);
+}
+//endregion
+
+function validerSaisie(){
+    let elInput = document.getElementById('btn1');
+    let val = elInput.value;
+    console.log('La valeur saisie est %s', val);
+
+    //Listener de façon programmative
+    document.querySelector('#btn2').addEventListener('click', clickBouton2);
+}
+
+function clickBouton2(event){
+    console.log('Clic sur le bouton 2' + event);
+    alert('Clic bouton 2');
+}
+
+function ajouterLien(){
+    var lien = document.createElement('a');
+    lien.href = 'https://www.eni-ecole.fr';
+    lien.target = '_blank';
+    lien.innerText = 'ENI';
+    document.getElementById('btn3').insertAdjacentElement('afterend', lien);
+}
+
+function colorerBouton(){
+    //tableau de boutons input
+    let boutons = document.querySelectorAll('input[type="button"]');
+    for(let bouton of boutons){
+        bouton.className = 'couleur';
+        //bouton.classList.add('couleur');
+    }
+}
